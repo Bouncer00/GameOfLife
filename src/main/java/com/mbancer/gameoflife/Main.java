f7654f2144232619aeccc45e1ff5f1da451c4c51
Mariusz Bańcer <mbancer@pgs-soft.com>
2017-05-11 14:08:53 +0200

    Optimizing imports

--- src/main/java/com/mbancer/gameoflife/Main.java
+++ src/main/java/com/mbancer/gameoflife/Main.java
@@ -1,8 +1,5 @@
 package com.mbancer.gameoflife;
 
-import com.mbancer.gameoflife.model.Generation;
-import com.mbancer.gameoflife.presenter.ConsoleGenerationPresenter;
-import com.mbancer.gameoflife.rules.RulesService;
 import com.mbancer.gameoflife.web.Server;
 
 import java.io.IOException;
